50bc3b0d2787ec147966cce0375230da6ec39d46
Naveen Vangalapudi <vangalapudinaveen.dev@gmail.com>
2025-10-04 18:59:37 +0530

    modifed version @1.0

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+// src/index.ts
+export { default as Announcement } from "./Announcement";
+export * from "./types/AnnouncementTypes";
+
+export { default as CallToAction } from "./CallToAction";
+export * from "./types/CallToActionTypes";
+
+export { default as Features } from "./Features";
+export * from "./types/FeaturesTypes";
+export * from "./types/FeaturesArrayTypes";
+
+export { default as Footer } from "./Footer";
+export * from "./types/FooterTypes";
+export * from "./types/PoliciesTypes";
+
+export { default as Hero } from "./Hero";
+export * from "./types/HeroTypes";
+
+export { default as Navbar } from "./Navbar";
+export * from "./types/NavbarTypes";
+export * from "./types/NavItemsTypes";
+
+export { default as Painpoints } from "./Painpoints";
+export * from "./types/PainpointsTypes";
+export * from "./types/PointsTypes";
+
+export { default as Pricing } from "./Pricing";
+export * from "./types/PricingTypes";
+export * from "./types/PricingArraytypes";
+
+export { default as Problem } from "./Problem";
+export * from "./types/ProblemTypes";
+
+export { default as Reviews } from "./Reviews";
+export * from "./types/ReviewTypes";
+export * from "./types/ReviewArrayTypes";
